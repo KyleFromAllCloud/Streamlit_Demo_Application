07c038eaa22898444e4d52739db312d3c3af8970
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-03-05 14:58:08 -0500

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -22,89 +22,87 @@ authenticator = stauth.Authenticate(
 
 name, authentication_status, username = authenticator.login('Login', 'main')
 
-if authentication_status:
+if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
-    st.title('Some content')
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+    TYPE_MAPPINGS = {
+        'int64': 'integer',
+        'object': 'string'
+    }
 
-TYPE_MAPPINGS = {
-    'int64': 'integer',
-    'object': 'string'
-}
-
-arr = np.array(['X', 'B', 'C'])
-# st.write(arr)
-if arr.dtype == object:
-    print((arr == None).any())
+    arr = np.array(['X', 'B', 'C'])
+    # st.write(arr)
+    if arr.dtype == object:
+        print((arr == None).any())
 
 
-class Requirement:
-    @staticmethod
-    def not_null(arr: np.ndarray):
-        li = arr.tolist()
-        return not np.NaN in li
+    class Requirement:
+        @staticmethod
+        def not_null(arr: np.ndarray):
+            li = arr.tolist()
+            return not np.NaN in li
 
 
-class CsvColumn(BaseModel):
-    name: str
-    type: Literal['string', 'integer']
-    requirements: list[str] = []
+    class CsvColumn(BaseModel):
+        name: str
+        type: Literal['string', 'integer']
+        requirements: list[str] = []
 
 
-class CsvSpec(BaseModel):
-    columns: list[CsvColumn]
+    class CsvSpec(BaseModel):
+        columns: list[CsvColumn]
 
 
-data_dict = yaml.safe_load(Path('csv_spec.yml').open('r'))
-csv_spec = CsvSpec.parse_obj(data_dict)
+    data_dict = yaml.safe_load(Path('csv_spec.yml').open('r'))
+    csv_spec = CsvSpec.parse_obj(data_dict)
 
 
-conn = connector.connect(**st.secrets["snowflake"])
+    conn = connector.connect(**st.secrets["snowflake"])
 
 
-st.title('Upload CSV Demo')
-st.header("File Specification")
-st.write(data_dict)
+    st.title('Upload CSV Demo')
+    st.header("File Specification")
+    st.write(data_dict)
 
-df = pd.DataFrame()
+    df = pd.DataFrame()
 
-uploaded_file = st.file_uploader('Upload a file')
-if uploaded_file is not None:
-    # read csv
-    df = pd.read_csv(uploaded_file)
+    uploaded_file = st.file_uploader('Upload a file')
+    if uploaded_file is not None:
+        # read csv
+        df = pd.read_csv(uploaded_file)
 
-# Validate File
-st.header("File Validation")
-is_valid = True
-for col in csv_spec.columns:
-    if col.name not in df.columns:
-        st.error(f"Column {col.name} is missing.")
-        is_valid = False
-    else:
-        col_data = df[col.name]
-        if TYPE_MAPPINGS.get(str(col_data.dtype), 'XXX') != col.type:
-            st.error(f"Column {col.name} must be of type {col.type}.")
+    # Validate File
+    st.header("File Validation")
+    is_valid = True
+    for col in csv_spec.columns:
+        if col.name not in df.columns:
+            st.error(f"Column {col.name} is missing.")
             is_valid = False
-        for req in col.requirements:
-            pass_req = getattr(Requirement, req)(col_data)
-            if not pass_req:
-                st.error(f"Column {col.name} failed {req} requirement.")
+        else:
+            col_data = df[col.name]
+            if TYPE_MAPPINGS.get(str(col_data.dtype), 'XXX') != col.type:
+                st.error(f"Column {col.name} must be of type {col.type}.")
                 is_valid = False
+            for req in col.requirements:
+                pass_req = getattr(Requirement, req)(col_data)
+                if not pass_req:
+                    st.error(f"Column {col.name} failed {req} requirement.")
+                    is_valid = False
 
-st.write(df)
+    st.write(df)
 
 
-btn_press = st.button('Submit Change', disabled=not is_valid)
+    btn_press = st.button('Submit Change', disabled=not is_valid)
 
-if btn_press:
-    uploaded_cols = df.columns.to_list()
-    st.write(uploaded_cols)
+    if btn_press:
+        uploaded_cols = df.columns.to_list()
+        st.write(uploaded_cols)
 
-    if 'RequiredColumn' not in uploaded_cols:
-        st.write("Failed - Missing column 'RequiredColumn'")
-    else:
-        st.write("Loaded!")
+        if 'RequiredColumn' not in uploaded_cols:
+            st.write("Failed - Missing column 'RequiredColumn'")
+        else:
+            st.write("Loaded!")
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
