992a06ac8c2627f481651e995a0467ccf4965b3f
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-03-05 14:39:42 -0500

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -9,6 +9,26 @@ import yaml
 from pathlib import Path
 import streamlit_authenticator as stauth
 
+with open('./login_config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+    
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
 
 TYPE_MAPPINGS = {
     'int64': 'integer',
