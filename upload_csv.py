a25b2f1e207bdcb6e5c7b8a8dcb8f72615ba2cea
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-03-12 21:03:41 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -58,6 +58,7 @@ if st.session_state["authentication_status"]:
 
 
     conn = connector.connect(**st.secrets["snowflake"])
+    curs = conn.cursor()
 
 
     st.title('Upload CSV Demo')
@@ -65,7 +66,7 @@ if st.session_state["authentication_status"]:
     st.write(data_dict)
 
     df = pd.DataFrame()
-    df_editor = conn.execute('SELECT * FROM DEV_EDW_PSTG.DEMO_SCHEMA.STREAMLIT_ENTRY_DEMO').fetch_pandas_all()
+    df_editor = curs.execute('SELECT * FROM DEV_EDW_PSTG.DEMO_SCHEMA.STREAMLIT_ENTRY_DEMO').fetch_pandas_all()
     
     edited_df = st.experimental_data_editor(df_editor)
 
