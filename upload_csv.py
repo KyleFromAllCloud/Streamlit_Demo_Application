5012f073efcbe650a761810682a87f094b735503
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-03-12 21:24:56 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -66,7 +66,7 @@ if st.session_state["authentication_status"]:
     st.write(data_dict)
 
     df = pd.DataFrame()
-    df_editor = curs.execute('SELECT * FROM DEV_EDW_PSTG.DEMO_SCHEMA.STREAMLIT_ENTRY_DEMO').fetch_all()
+    df_editor = curs.execute('SELECT * FROM DEV_EDW_PSTG.DEMO_SCHEMA.STREAMLIT_ENTRY_DEMO').fetchall()
     
 #     edited_df = st.experimental_data_editor(df_editor)
     st.text(f'{df_editor}')
