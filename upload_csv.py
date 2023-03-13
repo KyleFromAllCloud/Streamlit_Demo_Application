24c813ebaf37f1f6f6042455fef1d7e4aac67826
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-03-12 21:17:45 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -68,8 +68,9 @@ if st.session_state["authentication_status"]:
     df = pd.DataFrame()
     df_editor = curs.execute('SELECT * FROM DEV_EDW_PSTG.DEMO_SCHEMA.STREAMLIT_ENTRY_DEMO').fetch_pandas_all()
     
-    edited_df = st.experimental_data_editor(df_editor)
-
+#     edited_df = st.experimental_data_editor(df_editor)
+    st.markdown(df_editor)
+    
     uploaded_file = st.file_uploader('Upload a file')
     if uploaded_file is not None:
         # read csv
