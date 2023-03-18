37ab2b869228a4695b135485073fb41359a235ad
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-03-18 19:00:51 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -207,29 +207,67 @@ if st.session_state["authentication_status"]:
     if st.button('Refresh'):
         st.experimental_rerun()
         
-    
-    df_file = pd.DataFrame()
-#     tot_sql = "SELECT * FROM DEV_EDW_PSTG.DEMO_SCHEMA.STREAMLIT_ENTRY_DEMO;"
+    TYPE_MAPPINGS = {
+    'int64': 'integer',
+    'object': 'string'
+    }
 
-#     cur = conn.cursor().execute(tot_sql)
-#     df_editor = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
-    
-#     edited_df = st.experimental_data_editor(df_editor)
-    
+    arr = np.array(['X', 'B', 'C'])
+    # st.write(arr)
+    if arr.dtype == object:
+        print((arr == None).any())
+
+
+    class Requirement:
+        @staticmethod
+        def not_null(arr: np.ndarray):
+            li = arr.tolist()
+            return not np.NaN in li
+
+
+    class CsvColumn(BaseModel):
+        name: str
+        type: Literal['string', 'integer']
+        requirements: list[str] = []
+
+
+    class CsvSpec(BaseModel):
+        columns: list[CsvColumn]
+
+
+    data_dict = yaml.safe_load(Path('csv_spec.yml').open('r'))
+    csv_spec = CsvSpec.parse_obj(data_dict)
+
+
+    conn = connector.connect(
+        user=os.environ['SNOWFLAKE_USER'],
+        password=os.environ['SNOWFLAKE_PASSWORD'],
+        account='baa92216.us-east-1',
+        role='STREAMLIT_DEVELOPER',
+        warehouse='WH_STREAMLIT_DEMO'
+    )
+
+
+    st.title('Upload CSV Demo')
+    st.header("File Specification")
+    st.write(data_dict)
+
+    df = pd.DataFrame()
+
     uploaded_file = st.file_uploader('Upload a file')
     if uploaded_file is not None:
         # read csv
-        df_file = pd.read_csv(uploaded_file)
+        df = pd.read_csv(uploaded_file)
 
     # Validate File
     st.header("File Validation")
     is_valid = True
     for col in csv_spec.columns:
-        if col.name not in df_file.columns:
+        if col.name not in df.columns:
             st.error(f"Column {col.name} is missing.")
             is_valid = False
         else:
-            col_data = df_file[col.name]
+            col_data = df[col.name]
             if TYPE_MAPPINGS.get(str(col_data.dtype), 'XXX') != col.type:
                 st.error(f"Column {col.name} must be of type {col.type}.")
                 is_valid = False
@@ -239,20 +277,67 @@ if st.session_state["authentication_status"]:
                     st.error(f"Column {col.name} failed {req} requirement.")
                     is_valid = False
 
-    st.write(df_file)
+    st.write(df)
 
 
     btn_press = st.button('Submit Change', disabled=not is_valid)
 
     if btn_press:
-        uploaded_cols = df_file.columns.to_list()
+        uploaded_cols = df.columns.to_list()
         st.write(uploaded_cols)
 
         if 'RequiredColumn' not in uploaded_cols:
             st.write("Failed - Missing column 'RequiredColumn'")
         else:
-            success, nchunks, nrows, _ = write_pandas(conn, df_file, 'STREAMLIT_ENTRY_DEMO')
             st.write("Loaded!")
+
+        
+    
+#     df_file = pd.DataFrame()
+# #     tot_sql = "SELECT * FROM DEV_EDW_PSTG.DEMO_SCHEMA.STREAMLIT_ENTRY_DEMO;"
+
+# #     cur = conn.cursor().execute(tot_sql)
+# #     df_editor = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
+    
+# #     edited_df = st.experimental_data_editor(df_editor)
+    
+#     uploaded_file = st.file_uploader('Upload a file')
+#     if uploaded_file is not None:
+#         # read csv
+#         df_file = pd.read_csv(uploaded_file)
+
+#     # Validate File
+#     st.header("File Validation")
+#     is_valid = True
+#     for col in csv_spec.columns:
+#         if col.name not in df_file.columns:
+#             st.error(f"Column {col.name} is missing.")
+#             is_valid = False
+#         else:
+#             col_data = df_file[col.name]
+#             if TYPE_MAPPINGS.get(str(col_data.dtype), 'XXX') != col.type:
+#                 st.error(f"Column {col.name} must be of type {col.type}.")
+#                 is_valid = False
+#             for req in col.requirements:
+#                 pass_req = getattr(Requirement, req)(col_data)
+#                 if not pass_req:
+#                     st.error(f"Column {col.name} failed {req} requirement.")
+#                     is_valid = False
+
+#     st.write(df_file)
+
+
+#     btn_press = st.button('Submit Change', disabled=not is_valid)
+
+#     if btn_press:
+#         uploaded_cols = df_file.columns.to_list()
+#         st.write(uploaded_cols)
+
+#         if 'RequiredColumn' not in uploaded_cols:
+#             st.write("Failed - Missing column 'RequiredColumn'")
+#         else:
+#             success, nchunks, nrows, _ = write_pandas(conn, df_file, 'STREAMLIT_ENTRY_DEMO')
+#             st.write("Loaded!")
 
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
