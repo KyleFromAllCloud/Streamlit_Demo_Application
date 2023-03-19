da60a287e6f4026ad8af7056e369b571dd2dc235
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-03-18 20:21:51 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -14,6 +14,7 @@ from snowflake.connector.pandas_tools import write_pandas
 from pytz import country_names
 from st_aggrid import AgGrid, GridUpdateMode, JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
+from snowflake.snowpark.session import Session
 
 # The code below is for the title and logo.
 # st.set_page_config(page_title="Dataframe with editable cells", page_icon="💾")
