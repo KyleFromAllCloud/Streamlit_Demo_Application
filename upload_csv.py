d49974663d0d5f54a70be3b2b771e2e5ecc9f9bd
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-04-29 16:28:13 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -105,7 +105,7 @@ def get_dataset():
     # load messages df
     df = session.table("FORECAST_RBC")
     col_list_trim = ['ACCOUNT', 'PORTFOLIO']
-    col_list = col_list_trim + months
+    col_list = col_list_trim + months_years
 #     col_list = col_list + ['EXISTINGCLIENTNEWLOGO']
     df = df[col_list]
 #     df.rename(columns={"JAN": "23-JAN","FEB": "23-FEB", "MAR": "23-MAR", "APR": "23-APR", 
