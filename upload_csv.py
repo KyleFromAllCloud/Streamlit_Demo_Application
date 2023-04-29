4d2a26fdd74ca953bfc9b61de77340d5cb6feb92
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-04-29 16:35:51 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -116,7 +116,7 @@ def get_dataset():
 #     df = df.rename(cols)
     return df
 dataset = get_dataset()
-dataset = pd.melt(dataset, id_vars = col_list_trim, value_vars = months_years)
+# dataset = pd.melt(dataset, id_vars = col_list_trim, value_vars = months_years)
 st.dataframe(dataset)
 with st.form("data_editor_form"):
     st.caption("Edit the dataframe below")
