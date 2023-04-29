51cd9568b3fa0b9a09f594cba090e4b486adf851
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2023-04-29 17:12:26 -0400

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -119,8 +119,8 @@ def get_dataset():
 dataset = get_dataset()
 # dataset_pd = session.table("FORECAST_RBC").to_pandas()
 dataset_pd = pd.DataFrame(dataset.collect())
-dataset_pd = pd.melt(dataset_pd, id_vars = col_list_trim, value_vars = list(dataset.columns)[2:])
-st.dataframe(dataset_pd)
+# dataset_pd = pd.melt(dataset_pd, id_vars = col_list_trim, value_vars = list(dataset.columns)[2:])
+st.dataframe(dataset)
 with st.form("data_editor_form"):
     st.caption("Edit the dataframe below")
     edited = st.experimental_data_editor(dataset, width=1500, num_rows="dynamic")
